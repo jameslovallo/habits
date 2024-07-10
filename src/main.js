82353379a1baf451cef7743655429dd7665a84ee
James Lovallo <james.lovallo@thrivent.com>
2024-07-10 15:40:30 -0400

    .

--- src/main.js
+++ src/main.js
@@ -4,31 +4,56 @@ import '/src/components/habits.js'
 import '/src/components/icon.js'
 import '/src/components/tasks.js'
 
-const h1 = document.querySelector('h1')
-let timeOfDay = new Date().getHours()
+const nav = document.querySelector('nav')
 
-if (timeOfDay < 12) {
-	timeOfDay = 'Morning'
-} else if (timeOfDay < 18) {
-	timeOfDay = 'Afternoon'
-} else {
-	timeOfDay = 'Evening'
+if (nav) {
+	nav.innerHTML = /* html */ `
+		<a href="/">
+			<mdi-icon name="user"></mdi-icon>
+			${await t('My Day')}
+		</a>
+		<a href="/friends.html">
+			<mdi-icon name="group"></mdi-icon>
+			${await t('Friends')}
+		</a>
+		<a href="/settings.html">
+			<mdi-icon name="cog"></mdi-icon>
+			${await t('Settings')}
+		</a>
+	`
 }
 
-h1.innerText = await t(`Good ${timeOfDay}, James`)
+const welcome = document.querySelector('#welcome')
 
-const p = document.querySelector('p')
-const getRandom = (min, max) => {
-	const minCeiled = Math.ceil(min)
-	const maxFloored = Math.floor(max)
-	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+if (welcome) {
+	let timeOfDay = new Date().getHours()
+
+	if (timeOfDay < 12) {
+		timeOfDay = 'Morning'
+	} else if (timeOfDay < 18) {
+		timeOfDay = 'Afternoon'
+	} else {
+		timeOfDay = 'Evening'
+	}
+
+	welcome.innerText = await t(`Good ${timeOfDay}, James`)
 }
 
-fetch(
-	'https://raw.githubusercontent.com/BennettElisa/black-women-inspire/main/quotes.json'
-)
-	.then((res) => res.json())
-	.then(async (json) => {
-		const { quote, author } = json[getRandom(0, json.length - 1)]
-		p.innerHTML = `${await t(quote)} - ${author}`
-	})
+const quotation = document.querySelector('#quotation')
+
+if (quotation) {
+	const getRandom = (min, max) => {
+		const minCeiled = Math.ceil(min)
+		const maxFloored = Math.floor(max)
+		return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+	}
+
+	fetch(
+		'https://raw.githubusercontent.com/BennettElisa/black-women-inspire/main/quotes.json'
+	)
+		.then((res) => res.json())
+		.then(async (json) => {
+			const { quote, author } = json[getRandom(0, json.length - 1)]
+			quotation.innerHTML = `${await t(quote)} - ${author}`
+		})
+}
