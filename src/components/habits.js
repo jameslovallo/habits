1f02fb14599d841b6147121c028cc88728c2e6f1
James Lovallo <james.lovallo@thrivent.com>
2024-07-22 22:54:20 -0400

    .

--- src/components/habits.js
+++ src/components/habits.js
@@ -55,7 +55,7 @@ create('habit', {
 											part="text"
 											@click=${() => {
 												_app.record = id
-												_app.page = 'edit-habit'
+												setTimeout(() => (_app.page = 'edit-habit'))
 											}}
 										>
 											${Name}
